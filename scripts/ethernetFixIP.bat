3a268d92da8f5af440b050e531f1ad4b2513f3be
Balázs Gunics <gunicsba@gmail.com>
2025-12-18 18:31:39 +0100

    publish ethernetFixIP.bat

---
+++ scripts/ethernetFixIP.bat
@@ -0,0 +1,8 @@
+netsh interface ipv4 set address name="Ethernet" static 192.168.5.101 255.255.255.0 192.168.5.1
+netsh interface ipv4 set address name="Ethernet 1" static 192.168.5.102 255.255.255.0 192.168.5.1
+netsh interface ipv4 set address name="Ethernet 2" static 192.168.5.103 255.255.255.0 192.168.5.1
+netsh interface ipv4 set address name="Ethernet 3" static 192.168.5.105 255.255.255.0 192.168.5.1
+netsh interface ipv4 set address name="Ethernet 4" static 192.168.5.106 255.255.255.0 192.168.5.1
+netsh interface ipv4 set address name="Ethernet 5" static 192.168.5.108 255.255.255.0 192.168.5.1
+netsh interface ipv4 set address name="Ethernet 6" static 192.168.5.109 255.255.255.0 192.168.5.1
+pause
